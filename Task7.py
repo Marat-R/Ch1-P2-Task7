fadeda23ff5fe6f8d6e2139ed061dabf9b55cace
Marat-R <you@example.com>
2019-12-21 12:49:51 +0600

    first commit

---
+++ Task7.py
@@ -0,0 +1,11 @@
+
+X = int(input("Number: "))
+
+if X > 0:
+    print("1 - The number is positive")
+
+elif X < 0:
+    print("-1 - The number is negative")
+
+else:
+    print("0 - The number is equal to zero")
